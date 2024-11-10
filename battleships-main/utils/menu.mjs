57241964324cabaac5613f6e68f4e14ada764c01
TobbiRL <torbjornrl@uia.no>
2024-11-10 01:41:36 +0100

    figured I would need to make another menu for the language

--- battleships-main/utils/menu.mjs
+++ battleships-main/utils/menu.mjs
@@ -64,6 +64,62 @@ function createMenu(menuItems) {
     }
 }
 
+function createLanguageMenu(menuItems) {
+    return {
+        isDrawn: false,
+        next: null,
+        transitionTo: null,
+
+        update: function (dt) {
+            if (KeyBoardManager.isUpPressed()) {
+                currentActiveMenuItem--;
+                if (currentActiveMenuItem < 0) {
+                    currentActiveMenuItem = 0;
+                }
+                this.isDrawn = false;
+            }
+            else if (KeyBoardManager.isDownPressed()) {
+                currentActiveMenuItem++;
+                if (currentActiveMenuItem >= menuItems.length) {
+                    currentActiveMenuItem = menuItems.length - 1;
+                }
+                this.isDrawn = false;
+            }
+            else if (KeyBoardManager.isEnterPressed()) {
+                if (menuItems[currentActiveMenuItem].action) {
+                    menuItems[currentActiveMenuItem].action();
+                }
+            }
+        },
+
+        draw: function () {
+            if (this.isDrawn == false) {
+                this.isDrawn = true;
+                clearScreen();
+                let output = ""
+
+                for (let index in menuItems) {
+                    let menuItem = menuItems[index]
+
+                    let title = menuItem.text;
+                    if (currentActiveMenuItem == menuItem.id) {
+                        title = `*${menuItem.text}*`;
+                    } else {
+                        title = ` ${menuItem.text} `;
+                    }
+
+                    output += title + "\n";
+
+                }
+
+                printCenterd(output);
+
+            }
+        }
+
+    }
+}
+
 
 
 export default createMenu;
