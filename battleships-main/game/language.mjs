769cac9f324ec29230ed5470092c5897d0ad3029
TobbiRL <torbjornrl@uia.no>
2024-11-10 01:13:36 +0100

    made a file for language, and started adding language const's

---
+++ battleships-main/game/language.mjs
@@ -0,0 +1,25 @@
+const LANGUAGE = {
+
+en: {
+
+START_GAME: "Start Game",
+SHIP_PLACEMENT_START_PLAYER_ONE: `SHIP PLACMENT\nFirst player get ready.\nPlayer two look away`,
+SHIP_PLACEMENT_START_PLAYER_TWO: `SHIP PLACMENT\nSecond player get ready.\nPlayer one look away`,
+EXIT_GAME: "Exit Game",
+LANGUAGE_SETTINGS: "Language Settings",
+
+},
+
+no: {
+
+START_GAME: "Start spill",
+SHIP_PLACEMENT_START_PLAYER_ONE: `SKIP PLASSERING\nSpiller 1 gjør deg klar.\nSpiller 2 se vekk`,
+SHIP_PLACEMENT_START_PLAYER_TWO: `SKIP PLASSERING\nSpiller 2 gjør deg klar.\nSpiller 1 se vekk`,
+EXIT_GAME: "Slutt spill",
+LANGUAGE_SETTINGS: "Språk Instillinger",
+
+}
+
+}
+
+export default LANGUAGE
